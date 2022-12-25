a6a756bce8ec6b4df340c6ea3e3c1e721d7810a2
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-25 11:32:13 -0300

    Poo JavaScript

---
+++ js/script.js
@@ -0,0 +1,18 @@
+class Produto {
+
+   constructor() {
+      this.id = 0
+      this.nomeProduto = '';
+      this.valor = 0;
+   }
+
+   adicionar() {
+      alert('Vamos adicionar')
+   }
+
+   excluir() {
+      alert('Item deletado')
+   }
+}
+
+let produto = new Produto()
